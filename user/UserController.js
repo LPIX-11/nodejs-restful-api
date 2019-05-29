73ae2c9a0f2bd439b131444a363c09079c664044
Mohamed Johnson <mohamedjohnson31@gmail.com>
2019-05-29 20:15:39 +0000

    Minor Bbugs Fixed

--- user/UserController.js
+++ user/UserController.js
@@ -2,17 +2,19 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 router.use(bodyParser.json());
 var User = require('./User');
 
 // Create a new user [Route: /api/auth/register]
 router.post('/', function (req, res) {
     User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    },
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        },
         function (err, user) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(user);
@@ -46,7 +48,9 @@ router.delete('/:id', function (req, res) {
 
 // Updates the specified user [Route: /users/:id]
 router.put('/:id', function (req, res) {
-    User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
+    User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    }, function (err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
     });
