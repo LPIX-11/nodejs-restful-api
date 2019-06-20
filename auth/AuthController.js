defc35620d25ddb50a09b61b171f4a6eed3fe43f
Mohamed Johnson <mohamedjohnson31@gmail.com>
2019-06-20 14:42:09 +0000

    Code Smells Cleared

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -20,7 +20,7 @@ router.use(bodyParser.json());
 var User = require("../user/User");
 
 // Register new user
-router.post('/register', function (req, res) {
+router.post("/register", function (req, res) {
 
     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
     User.create({
@@ -70,9 +70,6 @@ router.get("/me", function (req, res) {
                 message: "Failed to authenticate token."
             });
         }
-        // Send Encoded user
-
-        // res.status(200).send(decoded);
 
         // Send Full Clear user informations
 
@@ -151,18 +148,18 @@ router.post("/login", function (req, res) {
 });
 
 // Update User Informations
-router.patch('/edit', function (req, res) {
-    var token = req.headers['x-access-token'];
+router.patch("/edit", function (req, res) {
+    var token = req.headers["x-access-token"];
 
     jwt.verify(token, config.secret, function (err, decoded) {
         if (!token) return res.status(401).send({
             auth: false,
-            message: 'No token provided.'
+            message: "No token provided."
         });
 
         if (err) return res.status(500).send({
             auth: false,
-            message: 'Failed to authenticate token.'
+            message: "Failed to authenticate token."
         });
 
         if (!req.body.email) {
