ed8f29dcde7a292e154479701cb2c19e66e7d376
Mohamed Johnson <mohamedjohnson31@gmail.com>
2019-06-20 15:24:22 +0000

    Code Smells Corrected

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var app = require("./app");
 var port = process.env.PORT || 3000;
 
-var server = app.listen(port, function () {
-  console.log(`Express server listening on port ${port}\nhttp://localhost:${port}`);
-});
+app.listen(port);
