33f43eb3180189d6915aee91f5a5e7180e349290
Mohamed Johnson <mohamedjohnson31@gmail.com>
2019-06-20 14:58:00 +0000

    Code Smells Corrected

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -152,15 +152,19 @@ router.patch("/edit", function (req, res) {
     var token = req.headers["x-access-token"];
 
     jwt.verify(token, config.secret, function (err, decoded) {
-        if (!token) return res.status(401).send({
-            auth: false,
-            message: "No token provided."
-        });
+        if (!token) {
+            return res.status(401).send({
+                auth: false,
+                message: "No token provided."
+            });
+        }
 
-        if (err) return res.status(500).send({
-            auth: false,
-            message: "Failed to authenticate token."
-        });
+        if (err) {
+            return res.status(500).send({
+                auth: false,
+                message: "Failed to authenticate token."
+            });
+        }
 
         if (!req.body.email) {
             res.status(401).send({
