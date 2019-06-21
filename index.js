631d598c685dd9e8657e2d13c15a34c2b44f7538
Mohamed Johnson <mohamedjohnson31@gmail.com>
2019-06-21 13:14:00 +0000

    Logs Manager Added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 var db = require("./db");
+const Log = require("./util/log");
 const port = process.env.PORT || 3000;
 
 
@@ -10,5 +11,5 @@ app.use("/users", UserController);
 var AuthController = require("./app/auth/authentification-controller");
 app.use("/auth", AuthController);
 
-app.listen(port, () => (console.log(`Nodejs Restful API running in ${port}`)));
+app.listen(port, () => Log.i(`Nodejs Restful API running in ${port}`));
 module.exports = app;
